9a0ae2f384e5111a7db88e7a1e1a3746a50242de
napathak <niraj@ox.security>
2024-03-04 22:21:03 +0530

    updated 04032024

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const axios = require('axios');
+const cors = require('cors');
 const users = require('./src/users');
 
 require('dotenv').config()
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Hello World')
