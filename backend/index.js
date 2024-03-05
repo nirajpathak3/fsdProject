80c68531d23fda57a0c1d42a699ae29d5ff846a6
napathak <niraj@ox.security>
2024-03-05 10:47:21 +0530

    050324 udpated

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+/// Logger Middleware is defined here
+const loggerMiddleware = (req, res, next) =>{
+  console.log("Hello...");
+  next();
+}
+app.use(loggerMiddleware);
+
+//// Routes are defined here
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/api/users', users.getUsers); // using to get all users
+app.get('/api/users', loggerMiddleware, users.getUsers); // using to get all users
 
+//// Server is started here
 app.listen(process.env.port, ()=>{
     console.log("Listening port...", process.env.port)
 })
